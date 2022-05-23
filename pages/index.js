e7fc89ee2835c66565d604549dca3fe3a60cbed9
Alex Bicknell <60597067+kuuzon@users.noreply.github.com>
2022-05-24 02:45:01 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,24 @@ function Home(props) {
   return (
     <div>
       <HeroSection title={'Code Feed'} description={'Catch up on all the coding news from around the globe, at the touch of a button ...'}/>
+      <NewsList loadedNews={props.news} />
     </div>
   );
 }
+
+// STATIC SITE GENERATION (snippet: "ngsp")
+export const getStaticProps = async () => {
+  // Fetch data from Internal API ("Code News")
+  const response = await axios.get(`${server}/api/news`);
+  const data = await response.data
+
+  // Returned data as props & ISR functionality
+  return {
+    props: {
+      news: data
+    },
+    revalidate: 60
+  };
+};
 
 export default Home;
